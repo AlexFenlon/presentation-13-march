a48ba14a0de4da8b9e5e6410ce9cbd056885924d
Alex Fenlon <a.fenlon@f5.com>
2024-03-11 08:49:29 +0000

    added changes to allow photos to be used from the directory, added photos, add comments, removed colouring due to not working

--- presentation.py
+++ presentation.py
@@ -1,52 +1,42 @@
 import os
 import subprocess
 import time
-from colorama import init, Fore, Style
-
-init(autoreset=True)
-
-delay = 0.04
+delay = 0.05
 
 def clear_terminal():
-    os.system("clear")
+    os.system("clear") # Mac only, replace with "cls" for Windows
 
 def ascii(file_location):
-    expanded_path = os.path.expanduser(file_location)
     try:
-        subprocess.run(["ascii_py", "-t", expanded_path])
+        subprocess.run(["ascii_py", "-t", file_location]) # found @ https://github.com/ProfOak/ascii_py/
     except FileNotFoundError:
-        print("Error: The 'ascii_py' command not found. Please make sure it's installed and in your PATH.")
+        print("Error: The 'ascii_py' command not found. Please install by using the command python3 -m pip install ascii_py to install ascii ")
 
     time.sleep(1)
 
 def type_text(text, delays):
-    colored_text = (
-        text.replace("[nginx]", f"{Fore.GREEN}[nginx]{Style.RESET_ALL}")
-        .replace("[f5]", f"{Fore.RED}[f5]{Style.RESET_ALL}")
-    )
-
-    for char in colored_text:
+    for char in text:
         print(char, end='', flush=True)
         time.sleep(delays)
     print()
 
 # Slide content
 slides = [
-    "My Journey So Far as an F5/NGINX Intern\n~/Downloads/wbr-NGINX-F5-AMA-1-featured-500x300-removebg-preview.png",
-    "Hi, I'm Alex! I am an intern at NGINX, a company owned by F5, based in Cork, Ireland. I am a student studying Software Development at MTU Cork. This is my journey so far in my first job as an intern in F5/NGNIX! \n~/Downloads/2052094765_256312f117_c.jpg",
+    "This is My Journey So Far as an F5/NGINX Intern\n/wbr-NGINX-F5-AMA-1-featured-500x300-removebg-preview.png", # file location for ascii image after \n/
+    "Hi, I'm Alex! I am an intern at NGINX, a company owned by F5, based in Cork, Ireland. I am a student studying Software Development at MTU Cork. This is my journey so far in my first job as an intern in F5/NGNIX! \n/2052094765_256312f117_c.png",
     "The first day was quite unique, I started with 2 other interns on the 1st of February which was a weird to start because it was a Thursday! It was also an early F5 day so big day to start. Met lots of new people, had a tour and even had a few games of pool! **How crazy is that?** We had pizza for lunch and a presentation afterwards.  **Has anyone else had an unusual start to their internship?**"
 ]
 
 # Display slides
 for slide_content in slides:
     clear_terminal()
-    if '\n~' in slide_content:
-        parts = slide_content.split('\n', 1)
+    if '\n/' in slide_content:
+        parts = slide_content.split('\n/', 1)
         type_text(parts[0], delay)
-        time.sleep(0.5)
+        time.sleep(1)
         clear_terminal()
         ascii(parts[1])
     else:
         type_text(slide_content, delay)
         if not slide_content.startswith("[ASCII"):
-            input("\nPress Enter to continue...")
+            input("\nPress Enter to continue...\n")
